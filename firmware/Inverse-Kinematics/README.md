0a8989981218a583ca782c0e65f0ad970a9b4a6d
Danila Loginov <danila@loginov.rocks>
2025-03-21 21:22:48 -0700

    firmware: Update Inverse Kinematics project

---
+++ firmware/Inverse-Kinematics/README.md
@@ -0,0 +1,3 @@
+# Inverse Kinematics
+
+TODO
--- firmware/Inverse-Kinematics/src/main.cpp
+++ firmware/Inverse-Kinematics/src/main.cpp
@@ -1,17 +1,12 @@
 #include <Arduino.h>
 #include <meArm.h>
+// #include <MeArm.h>
 
-#define SERIAL_BAUDRATE 9600
-
-#define BASE_SERVO_PIN 9
-#define SHOULDER_SERVO_PIN 10
-#define ELBOW_SERVO_PIN 6
-#define GRIPPER_SERVO_PIN 11
-
-meArm arm(137, 47, -pi / 4, pi / 4,    // base
-          133, 39, pi / 4, 3 * pi / 4, // shoulder
-          145, 94, 0, -pi / 4,         // elbow
-          80, 120, pi / 2, 0);         // gripper
+meArm arm(128, 33, -pi / 4, pi / 4,    // Base
+          135, 35, pi / 4, 3 * pi / 4, // Shoulder
+          135, 45, pi / 4, -pi / 4,    // Elbow
+          14, 41, pi / 2, 0);          // Claw
+// MeArm arm(...);
 
 String readBuffer = "";
 
@@ -21,113 +16,119 @@ void executeCoordinateCommand(String);
 
 void setup()
 {
-    Serial.begin(SERIAL_BAUDRATE);
+  Serial.begin(9600);
 
-    arm.begin(BASE_SERVO_PIN, SHOULDER_SERVO_PIN, ELBOW_SERVO_PIN, GRIPPER_SERVO_PIN);
+  // Base, Shoulder, Elbow, Claw pins.
+  arm.begin(6, 9, 10, 11);
 
-    Serial.println("MeArm initialized");
+  Serial.println("Setup complete");
+  Serial.println();
 }
 
 void loop()
 {
-    String input = getInput();
+  String input = getInput();
 
-    if (input.length() > 0)
+  if (input.length() > 0)
+  {
+    if (isCoordinateCommand(input))
     {
-        if (isCoordinateCommand(input))
-        {
-            executeCoordinateCommand(input);
-        }
-        else if (input == "g")
-        {
-            Serial.println("Opening the gripper");
-            arm.openGripper();
-        }
-        else if (input == "G")
-        {
-            Serial.println("Closing the gripper");
-            arm.closeGripper();
-        }
-        else if (input == "O")
-        {
-            Serial.println("Going to the home point");
-            arm.gotoPoint(0.0, 100.0, 50.0);
-        }
+      executeCoordinateCommand(input);
     }
+    else if (input == "c")
+    {
+      Serial.println("Opening the claw");
+      arm.openGripper();
+      // arm.openClaw();
+    }
+    else if (input == "C")
+    {
+      Serial.println("Closing the claw");
+      arm.closeGripper();
+      // arm.closeClaw();
+    }
+    else if (input == "O")
+    {
+      Serial.println("Going to the home point");
+      arm.gotoPoint(0.0, 100.0, 50.0);
+      // arm.moveToXYZ(0, 100, 50);
+    }
+  }
 }
 
 String getInput()
 {
-    String input = "";
+  String input = "";
 
-    while (Serial.available())
+  while (Serial.available())
+  {
+    char c = Serial.read();
+
+    if (c == '\n')
     {
-        char c = Serial.read();
-
-        if (c == '\n')
-        {
-            input = readBuffer;
-            input.trim();
-            readBuffer = "";
-        }
-        else if (c)
-        {
-            readBuffer += c;
-        }
+      input = readBuffer;
+      input.trim();
+      readBuffer = "";
     }
+    else if (c)
+    {
+      readBuffer += c;
+    }
+  }
 
-    return input;
+  return input;
 }
 
 boolean isCoordinateCommand(String command)
 {
-    return (command[0] == 'X' || command[0] == 'Y' || command[0] == 'Z');
+  return (command[0] == 'X' || command[0] == 'Y' || command[0] == 'Z');
 }
 
 void executeCoordinateCommand(String command)
 {
-    float x = arm.getX(),
-          y = arm.getY(),
-          z = arm.getZ();
+  float x = arm.getX(),
+        y = arm.getY(),
+        z = arm.getZ();
 
-    // Get float value from command string after the first character.
-    float val = command.substring(1).toFloat();
+  // Get float value from command string after the first character.
+  float val = command.substring(1).toFloat();
 
-    // Determine what coordinate will be changed.
-    switch (command[0])
-    {
-    case 'X':
-        x = val;
-        break;
+  // Determine what coordinate will be changed.
+  switch (command[0])
+  {
+  case 'X':
+    x = val;
+    break;
 
-    case 'Y':
-        y = val;
-        break;
+  case 'Y':
+    y = val;
+    break;
 
-    case 'Z':
-        z = val;
-        break;
+  case 'Z':
+    z = val;
+    break;
 
-    default:
-        return;
-    }
+  default:
+    return;
+  }
 
-    Serial.print("Going to the point (");
-    Serial.print(x);
-    Serial.print(", ");
-    Serial.print(y);
-    Serial.print(", ");
-    Serial.print(z);
-    Serial.print(")");
+  Serial.print("Going to the point (");
+  Serial.print(x);
+  Serial.print(", ");
+  Serial.print(y);
+  Serial.print(", ");
+  Serial.print(z);
+  Serial.print(")");
 
-    if (arm.isReachable(x, y, z))
-    {
-        Serial.println();
-    }
-    else
-    {
-        Serial.println(", which is not reachable");
-    }
+  if (arm.isReachable(x, y, z))
+  {
+    Serial.println();
+  }
+  else
+  {
+    Serial.println(", which is not reachable");
+  }
 
-    arm.gotoPoint(x, y, z);
+  arm.gotoPoint(x, y, z);
+  // arm.moveToXYZ(x, y, z);
 }
