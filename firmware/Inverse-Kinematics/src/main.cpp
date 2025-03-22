019107082682a668cb935c6883042ec1cf45bdcb
Danila Loginov <danila@loginov.rocks>
2025-03-21 22:22:52 -0700

    firmware(Inverse Kinematics): Refactor to align with Calibrator implementation

--- firmware/Inverse-Kinematics/src/main.cpp
+++ firmware/Inverse-Kinematics/src/main.cpp
@@ -8,127 +8,190 @@ meArm arm(128, 33, -pi / 4, pi / 4,    // Base
           14, 41, pi / 2, 0);          // Claw
 // MeArm arm(...);
 
-String readBuffer = "";
+// readSerial helpers.
+const size_t readSerialBufferSize = 16; // Should be large enough to accommodate a command.
+char readSerialBuffer[readSerialBufferSize];
+size_t readSerialBufferIndex = 0;
 
-String getInput();
-boolean isCoordinateCommand(String);
-void executeCoordinateCommand(String);
+char *readSerial();
+void moveCartesian(char, int);
 
 void setup()
 {
   Serial.begin(9600);
+
+  Serial.println("Setting up...");
+  Serial.println();
 
   // Base, Shoulder, Elbow, Claw pins.
   arm.begin(6, 9, 10, 11);
 
   Serial.println("Setup complete");
   Serial.println();
+  Serial.println("Serial commands supported:");
+  Serial.println("  <axis> <coordinate> - move the arm to coordinate, for example:");
+  Serial.println("    X 0");
+  Serial.println("    Y 100");
+  Serial.println("    Z 50");
+  Serial.println("  OPEN - open the claw");
+  Serial.println("  CLOSE - close the claw");
+  Serial.println("  RESET - move the arm to default coordinates");
+  Serial.println();
 }
 
 void loop()
 {
-  String input = getInput();
+  // React to Serial input.
+  char *input = readSerial();
 
-  if (input.length() > 0)
+  if (input != nullptr)
   {
-    if (isCoordinateCommand(input))
+    Serial.print("< ");
+    Serial.println(input);
+
+    if (strcmp(input, "OPEN") == 0)
     {
-      executeCoordinateCommand(input);
-    }
-    else if (input == "c")
-    {
-      Serial.println("Opening the claw");
       arm.openGripper();
       // arm.openClaw();
+      Serial.println("Claw opened");
     }
-    else if (input == "C")
+    else if (strcmp(input, "CLOSE") == 0)
     {
-      Serial.println("Closing the claw");
       arm.closeGripper();
       // arm.closeClaw();
+      Serial.println("Claw closed");
     }
-    else if (input == "O")
+    else if (strcmp(input, "RESET") == 0)
     {
-      Serial.println("Going to the home point");
-      arm.gotoPoint(0.0, 100.0, 50.0);
+      arm.gotoPoint(0, 100, 50);
       // arm.moveToXYZ(0, 100, 50);
+      Serial.println("Arm moved to default coordinates: X = 0, Y = 100, Z = 50");
     }
+    else
+    {
+      char axis; // Should be large enough to accommodate the axis name.
+      int coordinate;
+
+      if (sscanf(input, "%c %d", &axis, &coordinate) == 2)
+      {
+        if (axis == 'X' || axis == 'Y' || axis == 'Z')
+        {
+          Serial.print("Moving arm to ");
+          Serial.print(axis);
+          Serial.print(" = ");
+          Serial.println(coordinate);
+
+          moveCartesian(axis, coordinate);
+        }
+        else
+        {
+          Serial.println("Unknown axis");
+        }
+      }
+      else
+      {
+        Serial.println("Unknown command");
+      }
+    }
+
+    Serial.println();
   }
 }
 
-String getInput()
+/**
+ * Reads characters from the serial input, ignoring leading and trimming trailing spaces. The function returns a
+ * null-terminated string of characters received up to the first newline character ('\n'). If no data is available or
+ * the input buffer is empty, it returns a null pointer.
+ */
+char *readSerial()
 {
-  String input = "";
-
   while (Serial.available())
   {
-    char c = Serial.read();
+    char inputChar = Serial.read();
 
-    if (c == '\n')
+    // Ignore leading spaces.
+    if (readSerialBufferIndex == 0 && isspace(inputChar))
     {
-      input = readBuffer;
-      input.trim();
-      readBuffer = "";
+      continue;
     }
-    else if (c)
+
+    Serial.print(inputChar);
+
+    // Exit when a new line is received.
+    if (inputChar == '\n')
     {
-      readBuffer += c;
+      // Trim trailing spaces.
+      while (readSerialBufferIndex > 0 && isspace(readSerialBuffer[readSerialBufferIndex - 1]))
+      {
+        readSerialBufferIndex--;
+      }
+
+      // Null-terminate the buffer and reset the index.
+      readSerialBuffer[readSerialBufferIndex] = '\0';
+      readSerialBufferIndex = 0;
+
+      return readSerialBuffer;
+    }
+
+    if (readSerialBufferIndex < readSerialBufferSize - 1)
+    {
+      readSerialBuffer[readSerialBufferIndex++] = inputChar;
     }
   }
 
-  return input;
+  return nullptr;
 }
 
-boolean isCoordinateCommand(String command)
+void moveCartesian(char axis, int coordinate)
 {
-  return (command[0] == 'X' || command[0] == 'Y' || command[0] == 'Z');
-}
-
-void executeCoordinateCommand(String command)
-{
+  // Get current coordinates as the library moving function requires all coordinates to be passed when invoking.
   float x = arm.getX(),
         y = arm.getY(),
         z = arm.getZ();
 
-  // Get float value from command string after the first character.
-  float val = command.substring(1).toFloat();
-
-  // Determine what coordinate will be changed.
-  switch (command[0])
+  switch (axis)
   {
   case 'X':
-    x = val;
+    x = coordinate;
     break;
-
   case 'Y':
-    y = val;
+    y = coordinate;
     break;
-
   case 'Z':
-    z = val;
+    z = coordinate;
     break;
-
   default:
+    Serial.println("Unknown axis");
     return;
   }
 
-  Serial.print("Going to the point (");
-  Serial.print(x);
-  Serial.print(", ");
-  Serial.print(y);
-  Serial.print(", ");
-  Serial.print(z);
-  Serial.print(")");
+  arm.gotoPoint(x, y, z);
+  // arm.moveToXYZ(x, y, z);
 
-  if (arm.isReachable(x, y, z))
+  // Note: the library will still attempt to move to the specified coordinates even when isReachable() returns false.
+  bool isReachable = arm.isReachable(x, y, z);
+
+  Serial.print("Arm ");
+  if (isReachable)
+  {
+    Serial.print("moved");
+  }
+  else
+  {
+    Serial.print("tried to move");
+  }
+  Serial.print(" to coordinates: X = ");
+  Serial.print(x);
+  Serial.print(", Y = ");
+  Serial.print(y);
+  Serial.print(", Z = ");
+  Serial.print(z);
+  if (isReachable)
   {
     Serial.println();
   }
   else
   {
-    Serial.println(", which is not reachable");
+    Serial.println(" - but these were calculated as unreachable");
   }
-
-  arm.gotoPoint(x, y, z);
-  // arm.moveToXYZ(x, y, z);
 }
